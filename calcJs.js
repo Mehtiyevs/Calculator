32ba6d23221d49185ef6661145f0539205d08b01
Mehtiyevs <145849792+Mehtiyevs@users.noreply.github.com>
2023-11-20 02:41:24 +0100

    New modifed Files
    the calculator code has modified

---
+++ calcJs.js
@@ -0,0 +1,47 @@
+document.getElementById('grid').onclick = box
+
+let strNum = ''
+let strSymbols = ''
+let strNumTwo = ''
+
+let result = ''
+
+function box   (elements)  {
+     
+     switch(elements.target.id) {
+          case 'symbols':
+               elements.target.innerText === "%" ?
+                    strSymbols = '/'  : strSymbols = elements.target.innerText   
+          
+               strNumTwo =  strNum               
+               strNum = ''
+                
+               break   
+
+          case 'delete':
+               strNum = ''
+               strNumTwo = ''
+               strSymbols = ''
+          
+               break
+
+          case 'equal':
+               result = strNumTwo + strSymbols + strNum
+               result = result.replace('=','')
+                    
+               result = eval(result)
+               document.getElementById('answer').innerHTML = result
+               
+               strNum = result
+               
+               strNumTwo = ""
+               strSymbols = ''
+          
+               break
+          
+          default:
+               strNum += elements.target.innerText  
+     }   
+
+     document.getElementById('answer').innerHTML = strNumTwo + strSymbols + strNum   
+}
